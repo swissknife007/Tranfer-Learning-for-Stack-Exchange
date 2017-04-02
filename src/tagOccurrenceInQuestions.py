c200c6f506c4377f080f28b9a7d9815d32f7af07
Abhishek Walia <aw3011@columbia.edu>
2017-04-02 17:33:14 -0400

    Statistics regarding occurence of tags in question for each domain

--- src/tagOccurrenceInQuestions.py
+++ src/tagOccurrenceInQuestions.py
@@ -3,6 +3,7 @@
 
 import csv
 from HTMLParser import HTMLParser
+from collections import defaultdict
 import sys  
 import re
 import os
@@ -10,6 +11,11 @@ import os
 # Change the default encoding
 reload(sys)  
 sys.setdefaultencoding('utf8')
+
+#Uncomment the following line to print logs
+# printCountInfo = True
+printCountInfo = False
+
 
 def getCountOfWordInSentence(sWord, sentence):
     count = 0
@@ -27,32 +33,71 @@ def cleanSentence(sentence):
     return sentence
     
 
-def countTagOccurenceInQuestion(questionName, questionContent, tags):
+def countTagOccurenceInQuestion(questionId, questionName, questionContent, tags, countTagsDictionary):
     questionName = cleanSentence(questionName)
     questionContent = cleanSentence(questionContent)
     tags = cleanSentence(tags) 
 
     individualTags = tags.split(' ')
 
-    for tag in individualTags:
-        print tag + " in " + questionName + "\n" + questionContent
-        print getCountOfWordInSentence(tag, questionName)
-        print getCountOfWordInSentence(tag, questionContent)
+    if printCountInfo == True:
+        print questionName + "\n" + questionContent + "\n"
 
-    print '---------------------------------'
+    numberOfTags = 0
+    numberOfTagsFoundInNameOrContent = 0
+
+    for tag in individualTags:
+        numberOfTags += 1
+        countInName = getCountOfWordInSentence(tag, questionName)
+        countInContent = getCountOfWordInSentence(tag, questionContent)
+        if countInName > 0 or countInContent > 0:
+            numberOfTagsFoundInNameOrContent += 1
+            
+        if printCountInfo == True:
+            print tag + " occurrence count:"
+            print "\t In question name:" + str(countInName)
+            print "\t In question content:" + str(countInContent)
+
+    if printCountInfo == True:
+        print 'Finished processing'
+        print '---------------------------------'
+    # Make an entry in dictionary for this questionId
+    countTagsDictionary[questionId] = (numberOfTagsFoundInNameOrContent, numberOfTags)
+
+def consolidateResults(countTagsDictionary, fileName):
+    percRanges = [(0 , 0), (.1, .25), (.26, .50), (.51, .75), (.76, 1.0)]
+    countArr = []
+    totalQuestions = 0
+    rangeDictionary = defaultdict(int)
+
+    for key in countTagsDictionary:
+        totalQuestions += 1
+        (numOfTagsFoundInQuestion, TotalTagsInQuestion) = countTagsDictionary[key]
+        perc = float(numOfTagsFoundInQuestion) / TotalTagsInQuestion
+        for (lower, upper) in percRanges:
+            if perc >= lower and perc <= upper:
+                rangeDictionary[(lower, upper)] += 1
+                break
+                
+    print "\n\nFilename: " + fileName
+    print "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+    print "Total number of questions:" + str(totalQuestions)
+    for key in percRanges:
+        print str(key) + ":" + str(float(rangeDictionary[key]) / totalQuestions)
+
+
 
 def parseFile(fileName):
+    countTagsDictionary = dict()
     with open(fileName) as f:
         reader = csv.reader(f)
-        print reader
         for row in reader:
-            questionName = row[1]
-            questionContent = row[2]
-            tags = row[3]
-            countTagOccurenceInQuestion(questionName, questionContent, tags)
+            countTagOccurenceInQuestion(row[0], row[1], row[2], row[3], countTagsDictionary)
+    consolidateResults(countTagsDictionary, fileName)
 
-#inputFileName = ['biology_processed.csv', 'cooking_processed.csv', 'crypto_processed.csv', 'diy_processed.csv', 'robotics_processed.csv', 'travel_processed.csv']
-inputFileName = ['biology_processed.csv']
+inputFileName = ['biology_processed.csv', 'cooking_processed.csv', 'crypto_processed.csv', 'diy_processed.csv', 'robotics_processed.csv', 'travel_processed.csv']
+#inputFileName = ['biology_processed.csv']
 
 for i in range(len(inputFileName)):
     parseFile('../ProcessedData/' + inputFileName[i])
+
