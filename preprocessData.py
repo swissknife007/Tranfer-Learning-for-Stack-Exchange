0dd7bd947a3101a36048204aaf3b8e64b220486a
Abhishek Walia <aw3011@columbia.edu>
2017-04-02 01:24:13 -0400

    Python script to parse csv and remove html tags (Preprocess)

---
+++ preprocessData.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+# encoding=utf8 
+
+import csv
+from HTMLParser import HTMLParser
+import sys  
+
+# Change the default encoding
+reload(sys)  
+sys.setdefaultencoding('utf8')
+
+# Keep relevant information from the HTML tags
+class HTMLSanitizer(HTMLParser):
+
+    def __init__(self, fileHdl):
+        HTMLParser.__init__(self)
+        self.currentTag = ''
+        self.fileHdl = fileHdl
+
+    def handle_starttag(self, tag, attrs):
+        self.currentTag = tag
+
+    def handle_endtag(self, tag):
+        None
+
+    def handle_data(self, data):
+        if self.currentTag != 'code':
+            self.fileHdl.write(data)
+
+
+
+def parseFile(fileName):
+    with open(fileName) as f:
+        print fileName
+        reader = csv.reader(f)
+        splitFileName = fileName.split('.')
+        splitFileName[0] = splitFileName[0] + '_processed'
+        processedFileName = '.'.join(splitFileName)
+        with open(processedFileName, "w") as processedF:
+            parser = HTMLSanitizer(processedF)
+            for row in reader:
+                processedF.write(row[0] + ', ')
+                processedF.write('"' + row[1] + '", "')
+                parser.feed(row[2])
+                processedF.write('", ' + row[3] + "\n")
+
+
+inputFileName = ['biology.csv', 'cooking.csv', 'crypto.csv', 'diy.csv', 'robotics.csv', 'travel.csv']
+
+for i in range(len(inputFileName)):
+    parseFile(inputFileName[i])
+
+
+
