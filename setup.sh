4b4f87d4de5204bcb75cefaf42233b53a7e02135
Abhishek Walia <aw3011@columbia.edu>
2017-04-25 15:17:42 -0400

    Generate tag files for each domain.

--- setup.sh
+++ setup.sh
@@ -23,6 +23,12 @@ do
     echo $FILE
     sed -i 's/ \+/ /g' $FILE
     sed -i '1d' $FILE
+    TAGFILE=`echo $FILE | cut -d'_' -f1`
+    cut -d',' -f4 $FILE > temp
+    sed -i 's/"//g' temp
+    sed -i 's/ /\n/g' temp
+    sort temp|uniq > $TAGFILE
+    rm temp
 done
 cd ..
 
